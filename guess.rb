cde19c1325726c5a2144c6c0f6d31d352b948e90
shaileygulrajani <shaileygulrajani@gmail.com>
2018-07-25 13:53:41 +0000

    automatically backed up by learn

---
+++ guess.rb
@@ -0,0 +1,16 @@
+def guess
+puts "Guess a number from 1 to 10"
+guess = 6
+guesser = gets.chomp.to_i
+
+while guesser != 6
+puts "Try again"
+
+if guesser == 6
+  puts "Correct!"
+
+
+end 
+end 
+end 
+guess 
